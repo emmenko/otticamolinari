a78be1973d2aacbe3c30bc75183012850a4b1b63
Nicola Molinari <nicola.molinari@commercetools.de>
2020-05-03 21:13:42 +0200

    refactor: render cookie consent only client side

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,61 +11,64 @@ import CookieConsent from "./cookie-consent";
 
 type LayoutProps = { children: React.ReactNode; className?: string };
 
-const Layout = (props: LayoutProps) => (
-  <Styled.root data-testid="theme-root">
-    <Global
-      // @ts-ignore
-      styles={css({
-        "*": {
-          boxSizing: `inherit`,
-        },
-        body: {
-          margin: 0,
-          padding: 0,
-          boxSizing: `border-box`,
-          textRendering: `optimizeLegibility`,
-        },
-        "::selection": {
-          backgroundColor: `primary`,
-          color: `white`,
-        },
-        a: {
-          transition: `all 0.3s ease-in-out`,
-          color: `text`,
-        },
-        figcaption: {
-          textAlign: "center",
-          fontSize: "0.875rem",
-        },
-        address: {
-          fontSize: [1, 2],
-        },
-      })}
-    />
-    <SEO />
-    <Box
-      sx={{
-        width: "100%",
-        minWidth: 0,
-        maxWidth: 1024,
-        mx: "auto",
-        p: 4,
-        variant: "styles.Container",
-      }}
-    >
-      <Header />
+const Layout = (props: LayoutProps) => {
+  const isClient = typeof window !== "undefined";
+  return (
+    <Styled.root data-testid="theme-root">
+      <Global
+        // @ts-ignore
+        styles={css({
+          "*": {
+            boxSizing: `inherit`,
+          },
+          body: {
+            margin: 0,
+            padding: 0,
+            boxSizing: `border-box`,
+            textRendering: `optimizeLegibility`,
+          },
+          "::selection": {
+            backgroundColor: `primary`,
+            color: `white`,
+          },
+          a: {
+            transition: `all 0.3s ease-in-out`,
+            color: `text`,
+          },
+          figcaption: {
+            textAlign: "center",
+            fontSize: "0.875rem",
+          },
+          address: {
+            fontSize: [1, 2],
+          },
+        })}
+      />
+      <SEO />
       <Box
-        id="skip-nav"
-        as="main"
-        sx={{ flex: "1 1 auto", variant: "styles.Main" }}
-        className={props.className}
+        sx={{
+          width: "100%",
+          minWidth: 0,
+          maxWidth: 1024,
+          mx: "auto",
+          p: 4,
+          variant: "styles.Container",
+        }}
       >
-        {props.children}
+        <Header />
+        <Box
+          id="skip-nav"
+          as="main"
+          sx={{ flex: "1 1 auto", variant: "styles.Main" }}
+          className={props.className}
+        >
+          {props.children}
+        </Box>
+        <Footer />
+        {isClient && <CookieConsent />}
       </Box>
-      <Footer />
-      <CookieConsent />
-    </Box>
-  </Styled.root>
-);
+    </Styled.root>
+  );
+};
 
 export default Layout;
