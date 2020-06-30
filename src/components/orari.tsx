5b3fefa22827f1f8db75c9e4979bb33488992ac0
Nicola Molinari <nicola.molinari@commercetools.de>
2020-06-30 11:09:25 +0200

    refactor: update timetable, add eyeshaker video (#31)

--- src/components/orari.tsx
+++ src/components/orari.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
+import React from "react";
 import { jsx, SxStyleProp } from "theme-ui";
-import { Grid, Text } from "@theme-ui/components";
+import { Grid, Text, Message } from "@theme-ui/components";
 
 type Props = {
   sx?: SxStyleProp;
@@ -23,47 +24,53 @@ const InlineGrid = (props) => (
 );
 
 const Orari = (props: Props) => (
-  <Grid
-    gap={[1, 1, 0]}
-    columns="150px 1fr"
-    sx={{
-      fontSize: 1,
-      justifyContent: "center",
-      alignItems: ["flex-start", "flex-start", "flex-start", "center"],
-      fontWeight: "bold",
-    }}
-    className={props.className}
-  >
-    <Text as="span">Lunedì</Text>
-    <Text as="span">chiuso</Text>
-    <Text as="span">Martedì</Text>
-    <InlineGrid>
-      <Text as="span">10:00 - 12:30</Text>
-      <Text as="span">16:00 - 19:00</Text>
-    </InlineGrid>
-    <Text as="span">Mercoledì</Text>
-    <InlineGrid>
-      <Text as="span">10:00 - 12:30</Text>
-      <Text as="span">16:00 - 19:00</Text>
-    </InlineGrid>
-    <Text as="span">Giovedì</Text>
-    <InlineGrid>
-      <Text as="span">10:00 - 12:30</Text>
-      <Text as="span">16:00 - 19:00</Text>
-    </InlineGrid>
-    <Text as="span">Venerdì</Text>
-    <InlineGrid>
-      <Text as="span">10:00 - 12:30</Text>
-      <Text as="span">16:00 - 19:00</Text>
-    </InlineGrid>
-    <Text as="span">Sabato</Text>
-    <InlineGrid>
-      <Text as="span">10:00 - 12:30</Text>
-      <Text as="span">16:00 - 19:00</Text>
-    </InlineGrid>
-    <Text as="span">Domenica</Text>
-    <Text as="span">chiuso</Text>
-  </Grid>
+  <React.Fragment>
+    <Grid
+      gap={[1, 1, 0]}
+      columns="150px 1fr"
+      sx={{
+        mt: 2,
+        fontSize: 1,
+        justifyContent: "center",
+        alignItems: ["flex-start", "flex-start", "flex-start", "center"],
+        fontWeight: "bold",
+      }}
+      className={props.className}
+    >
+      <Text as="span">Lunedì</Text>
+      <Text as="span">chiuso</Text>
+      <Text as="span">Martedì</Text>
+      <InlineGrid>
+        <Text as="span">10:00 - 12:30</Text>
+        <Text as="span">16:00 - 19:00</Text>
+      </InlineGrid>
+      <Text as="span">Mercoledì</Text>
+      <InlineGrid>
+        <Text as="span">10:00 - 12:30</Text>
+        <Text as="span">16:00 - 19:00</Text>
+      </InlineGrid>
+      <Text as="span">Giovedì</Text>
+      <InlineGrid>
+        <Text as="span">10:00 - 12:30</Text>
+        <Text as="span">16:00 - 19:00</Text>
+      </InlineGrid>
+      <Text as="span">Venerdì</Text>
+      <InlineGrid>
+        <Text as="span">10:00 - 12:30</Text>
+        <Text as="span">16:00 - 19:00</Text>
+      </InlineGrid>
+      <Text as="span">Sabato</Text>
+      <InlineGrid>
+        <Text as="span">10:00 - 12:30</Text>
+        <Text as="span">16:00 - 19:00</Text>
+      </InlineGrid>
+      <Text as="span">Domenica</Text>
+      <Text as="span">chiuso</Text>
+    </Grid>
+    <Message sx={{ mt: 2, fontSize: ".8rem" }}>
+      Sabato pomeriggio chiuso durante i mesi di Luglio e Agosto
+    </Message>
+  </React.Fragment>
 );
 
 export default Orari;
